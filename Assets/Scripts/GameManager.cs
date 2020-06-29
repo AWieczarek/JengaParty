a44168f9c2f831a5e02c74d49db381a972a291cf
AWieczarek <adamwie12@gmail.com>
2020-06-29 23:31:26 +0200

    Prepare for quest system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject QuestPanel;
+
+    public void Quest()
+    {
+        QuestPanel.SetActive(true);
+        StartCoroutine(Disable(5, QuestPanel));
+
+    }
+
+    IEnumerator Disable(float time, GameObject gm)
+    {
+        GameObject gameObject = gm;
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(false);
+    }
+
+}
